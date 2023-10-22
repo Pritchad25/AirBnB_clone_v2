679d2ac18c25b8bc49cce168fb5863e2755433f3
Pritchad25 <pricts29@gmail.com>
2023-10-22 17:38:51 +0000

    A Python Script that starts a Flask web application. In this case, we include the route /c/<text>.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This Python script starts a Flask web application:
+listening on 0.0.0.0, port 5000 Routes /: display “Hello HBNB!”
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home_hbnb():
+    """ This function defines the home route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ This function defines the hbnb route return HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>",  strict_slashes=False)
+def c_route(text):
+    """ This function displays “C ” followed by the value of the
+	text variable """
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
