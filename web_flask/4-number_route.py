8e059fbdceffec32d7011ba58ddbd02eca3a997d
Pritchad25 <pricts29@gmail.com>
2023-10-23 09:46:12 +0000

    displays n is a number if n is an integer.

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,34 +8,37 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def home_hbnb():
-    """ This function defines the home route """
+def home():
+    """This functiom displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """ This function defines the hbnb route return HBNB"""
+def hello_hbnb():
+    """The /hbnb route which displays HBNB"""
     return "HBNB"
 
-@app.route("/c/<text>",  strict_slashes=False)
-def c_route(text):
-    """ This function displays “C ” followed by the value of the
-	text variable """
-    return "C {}".format(text.replace("_", " "))
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_route(text="is cool"):
-    """ This function displays “Python ”, followed by the value of the
-	text variable """
-    return f"Python {text.replace('_', ' ')}"
+@app.route('/c/<text>', strict_slashes=False)
+def dynamic_route(text):
+    """This function displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return f"C {text}"
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_route(n):
-	""" Function displays a number if n is an integer. """
-    return "{} is a number".format(n)
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def dynamic_route_pyton(text="is cool"):
+    """This function displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ This function displays “n is a number” only if n is an integer"""
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
