5c8896f2675f78731814a23ba4be6fc3d652d76f
Pritchad25 <pricts29@gmail.com>
2023-10-24 14:29:13 +0000

    displays an HTML page inside the boyd.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,21 +4,15 @@ A Python script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 """
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
-
-@app.teardown_appcontext
-def teardown_appcontext(e):
-    storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    states = sorted(list(storage.all("State").values()), key=lambda x:x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
