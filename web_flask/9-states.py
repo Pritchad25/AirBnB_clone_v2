5575b8e72571b6a1dce168a06a61c2e18ec4e839
Pritchad25 <pricts29@gmail.com>
2023-10-24 15:52:19 +0000

    displays the list of City objects linked to the State sorted by name.

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,34 +4,31 @@ The Python script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 """
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(e):
-    """ This method removes the current SQLAlchemy Session after each
-	 request """
-    storage.close()
-
-
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def states(id=None):
-    """ This method displays a HTML page """
-    if id:
-        states = storage.all(State).values()
-        for state in states:
-            if state.id == id:
-                return render_template("9-states.html", state=state)
-        else:
-            return render_template("9-states.html", states=None)
-    else:
-        states = storage.all(State).values()
-        return render_template("7-states_list.html", states=states)
-
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<states_id>', strict_slashes=False)
+def states(state_id=None):
+    """ This method displays the states and cities listed in
+	alphabetical order."""
+	states = storage.all("State")
+	if state_id is not None:
+		state_id = 'State.' + state_id
+	return render_template('9-states.html', states=states,
+state_id=state_id))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
+@app.teardown_appcontext
+def teardown(e):
+	""" This method removes the current SQLAlchemy Session after each
+	request """
+	storage.close()
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
